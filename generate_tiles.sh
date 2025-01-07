13c3e8bf93194d51992ea22843b7bc933409c738
acalcutt <acalcutt@techidiots.net>
2025-01-07 00:17:38 -0500

    add bbox options

--- generate_tiles.sh
+++ generate_tiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Default Values (except demUrl and oDir)
+# Default Values
 verbose_default=false
 increment_default=0
 sMaxZoom_default=8
@@ -8,46 +8,53 @@ sEncoding_default="mapbox"
 oMaxZoom_default=8
 oMinZoom_default=5
 oDir_default=./output
+processes_default=8
 
+# Function to output usage information
 usage_message() {
     echo "Usage: $0 <function> [options]" >&2
     echo "" >&2
     echo "Functions:" >&2
-    echo "  generate-tile-pyramid  generates contours for a parent tile and all child tiles up to a specified max zoom level." >&2
-    echo "  generate-zoom-level    generates a list of parent tiles at a specifed zoom level, then runs generate-tile-pyramid on" >&2
-    echo "                         each of them in parallel" >&2
+    echo " pyramid   generates contours for a parent tile and all child tiles up to a specified max zoom level." >&2
+    echo " zoom      generates a list of parent tiles at a specifed zoom level, then runs pyramid on each of them in parallel" >&2
+    echo " bbox      generates a list of parent tiles inside a bounding box, then runs pyramid on each of them in parallel" >&2
     echo "" >&2
-    echo " Options for function generate-tile-pyramid:" >&2
-    echo "  --x <number>          The X coordinate of the parent tile." >&2
-    echo "  --y <number>          The Y coordinate of the parent tile." >&2
-    echo "  --z <number>          The Z coordinate of the parent tile." >&2
+    echo " General Options" >&2
     echo "  --demUrl <string>     The URL of the DEM source. (pmtiles://<http or local file path> or https://<zxyPattern>)" >&2
     echo "  --sEncoding <string>  The encoding of the source DEM tiles (e.g., 'terrarium', 'mapbox'). (default: $sEncoding_default)" >&2
     echo "  --sMaxZoom <number>   The maximum zoom level of the source DEM. (default: $sMaxZoom_default)" >&2
     echo "  --increment <number>  The contour increment value to extract. Use 0 for default thresholds." >&2
     echo "  --oMaxZoom <number>   The maximum zoom level of the output tile pyramid. (default: $oMaxZoom_default)" >&2
     echo "  --oDir <string>       The output directory where tiles will be stored. (default: $oDir_default)" >&2
+    echo "  --processes <number>  The number of parallel processes to use. (default: $processes_default)" >&2
     echo "" >&2
-    echo "Options for function generate-zoom-level:" >&2
-    echo "  --demUrl <string>     The URL of the DEM source. (pmtiles://<http or local file path> or https://<zxyPattern>)" >&2
-    echo "  --sEncoding <string>  The encoding of the source DEM tiles (e.g., 'terrarium', 'mapbox'). (default: $sEncoding_default)" >&2
-    echo "  --sMaxZoom <number>   The maximum zoom level of the source DEM. (default: $sMaxZoom_default)" >&2
-    echo "  --increment <number>  The contour increment value to extract. Use 0 for default thresholds." >&2
-    echo "  --oMaxZoom <number>   The maximum zoom level of the output tile pyramid. (default: $oMaxZoom_default)" >&2
+    echo " Additional Required Options for 'pyramid':" >&2
+    echo "  --x <number>    The X coordinate of the parent tile." >&2
+    echo "  --y <number>    The Y coordinate of the parent tile." >&2
+    echo "  --z <number>    The Z coordinate of the parent tile." >&2
+    echo "" >&2
+    echo " Additional Required Options for 'zoom':" >&2
     echo "  --oMinZoom <number>   The minimum zoom level of the output tile pyramid. (default: $oMinZoom_default)" >&2
-    echo "  --oDir <string>       The output directory where tiles will be stored. (default: $oDir_default)" >&2
+    echo "" >&2
+    echo " Additional Required Options for 'bbox':" >&2
+    echo "  --minx <number>   The minimum X coordinate of the bounding box." >&2
+    echo "  --miny <number>   The minimum Y coordinate of the bounding box." >&2
+    echo "  --maxx <number>   The maximum X coordinate of the bounding box." >&2
+    echo "  --maxy <number>   The maximum Y coordinate of the bounding box." >&2
+    echo "  --oMinZoom <number>   The minimum zoom level of the output tile pyramid. (default: $oMinZoom_default)" >&2
     echo "" >&2
     echo "  -v|--verbose  Enable verbose output" >&2
-    echo "  -h|--help  Show this usage statement" >&2
+    echo "  -h|--help Show this usage statement" >&2
     echo "" >&2
 }
 
+# Function to handle usage and exit
 usage() {
     usage_message
     exit 1
 }
 
-# Function to parse command line arguments for function generate-tile-pyramid
+# Function to parse command line arguments for the 'pyramid' function
 parse_arguments_option_1() {
     local verbose="$verbose_default"
     local x=""
@@ -59,6 +66,7 @@ parse_arguments_option_1() {
     local increment=""
     local oMaxZoom="$oMaxZoom_default"
     local oDir="$oDir_default"
+    local processes="$processes_default"
 
     while [[ $# -gt 0 ]]; do
         case "$1" in
@@ -71,6 +79,7 @@ parse_arguments_option_1() {
             --increment) increment="$2"; shift 2;;
             --oMaxZoom) oMaxZoom="$2"; shift 2;;
             --oDir) oDir="$2"; shift 2;;
+            --processes) processes="$2"; shift 2;;
             -h|--help) usage; exit 1;;
             -v|--verbose) verbose=true; shift ;;
             *) echo "Unknown option: $1" >&2; usage; exit 1;;
@@ -79,9 +88,9 @@ parse_arguments_option_1() {
 
     # Check if all required values are provided
     if [[ -z "$x" || -z "$y" || -z "$z" || -z "$demUrl" || -z "$increment" ]]; then
-        echo "Error: --x, --y, --z, --demUrl, and --increment are required for function generate-tile-pyramid." >&2
+        echo "Error: --x, --y, --z, --demUrl, and --increment are required for function pyramid." >&2
         usage_message
-        echo "Error: --x, --y, --z, --demUrl, and --increment are required for function generate-tile-pyramid." >&2
+        echo "Error: --x, --y, --z, --demUrl, and --increment are required for function pyramid." >&2
         exit 1
     fi
 
@@ -92,11 +101,11 @@ parse_arguments_option_1() {
         exit 1 # Return non-zero on error
     fi
 
-    echo "$x $y $z $demUrl $sEncoding $sMaxZoom $increment $oMaxZoom $oDir $verbose"
+    echo "$x $y $z $demUrl $sEncoding $sMaxZoom $increment $oMaxZoom $oDir $verbose $processes"
     return 0
 }
 
-# Function to parse command line arguments for function generate-zoom-level
+# Function to parse command line arguments for the 'zoom' function
 parse_arguments_option_2() {
     local verbose="$verbose_default"
     local demUrl=""
@@ -106,19 +115,21 @@ parse_arguments_option_2() {
     local sEncoding="$sEncoding_default"
     local oMaxZoom="$oMaxZoom_default"
     local oMinZoom="$oMinZoom_default"
+    local processes="$processes_default"
 
     while [[ $# -gt 0 ]]; do
         case "$1" in
-        -h|--help) usage; exit 1;; # Show usage and exit
-        --increment) increment="$2"; shift 2 ;;
-        --sMaxZoom) sMaxZoom="$2"; shift 2 ;;
-        --sEncoding) sEncoding="$2"; shift 2 ;;
-        --demUrl) demUrl="$2"; shift 2 ;;
-        --oDir) oDir="$2"; shift 2 ;;
-        --oMaxZoom) oMaxZoom="$2"; shift 2 ;;
-        --oMinZoom) oMinZoom="$2"; shift 2 ;;
-        -v|--verbose) verbose=true; shift ;;
-        *) echo "Unknown option: $1" >&2; usage; exit 1;; # Return non-zero on error
+            -h|--help) usage; exit 1;; # Show usage and exit
+            --increment) increment="$2"; shift 2 ;;
+            --sMaxZoom) sMaxZoom="$2"; shift 2 ;;
+            --sEncoding) sEncoding="$2"; shift 2 ;;
+            --demUrl) demUrl="$2"; shift 2 ;;
+            --oDir) oDir="$2"; shift 2 ;;
+            --oMaxZoom) oMaxZoom="$2"; shift 2 ;;
+            --oMinZoom) oMinZoom="$2"; shift 2 ;;
+            --processes) processes="$2"; shift 2;;
+            -v|--verbose) verbose=true; shift ;;
+            *) echo "Unknown option: $1" >&2; usage; exit 1;; # Return non-zero on error
         esac
     done
 
@@ -137,34 +148,77 @@ parse_arguments_option_2() {
     fi
 
     # Return the values as a single string
-    echo "$oMinZoom $demUrl $oDir $increment $sMaxZoom $sEncoding $oMaxZoom $verbose"
+    echo "$oMinZoom $demUrl $oDir $increment $sMaxZoom $sEncoding $oMaxZoom $verbose $processes"
     return 0 # return zero for success
 }
 
-# Function 1: Generate all tiles under parent tile
-run_option_1() {
-    local programOptions="$1"
-    read x y z demUrl sEncoding sMaxZoom increment oMaxZoom oDir verbose <<<"$programOptions"
+# Function to parse command line arguments for the 'bbox' function
+parse_arguments_option_3() {
+    local minx=""
+    local miny=""
+    local maxx=""
+    local maxy=""
+    local demUrl=""
+    local sEncoding="$sEncoding_default"
+    local sMaxZoom="$sMaxZoom_default"
+    local increment="$increment_default"
+    local oMinZoom="$oMinZoom_default"
+    local oMaxZoom="$oMaxZoom_default"
+    local oDir="$oDir_default"
+    local verbose="$verbose_default"
 
-    if [[ "$verbose" = "true" ]]; then
-        echo "process_tile: [START] Processing tile - Zoom: $z, X: $x, Y: $y, oMaxZoom: $oMaxZoom"
+    local processes="$processes_default"
+
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            --minx) minx="$2"; shift 2;;
+            --miny) miny="$2"; shift 2;;
+            --maxx) maxx="$2"; shift 2;;
+            --maxy) maxy="$2"; shift 2;;
+            --demUrl) demUrl="$2"; shift 2 ;;
+            --increment) increment="$2"; shift 2 ;;
+            --sEncoding) sEncoding="$2"; shift 2 ;;
+            --sMaxZoom) sMaxZoom="$2"; shift 2 ;;
+            --oMinZoom) oMinZoom="$2"; shift 2 ;;
+            --oMaxZoom) oMaxZoom="$2"; shift 2 ;;
+            --oDir) oDir="$2"; shift 2 ;;
+            --processes) processes="$2"; shift 2;;
+            -h|--help) usage; exit 1;; # Show usage and exit
+            -v|--verbose) verbose=true; shift ;;
+            *) echo "Unknown option: $1" >&2; usage; exit 1;; # Return non-zero on error
+        esac
+    done
+
+    if [[ -z "$minx" || -z "$miny" || -z "$maxx" || -z "$maxy" || -z "$demUrl" ]]; then
+        echo "Error: --minx, --miny, --maxx, --maxy, and --demUrl are required for function bbox." >&2
+        usage_message
+        exit 1
     fi
 
-    npx tsx ./src/generate-countour-tile-pyramid.ts --x "$x" --y "$y" --z "$z" --demUrl "$demUrl" --sEncoding "$sEncoding" --sMaxZoom "$sMaxZoom" --increment "$increment" --oMaxZoom "$oMaxZoom" --oDir "$oDir"
-
-    if [[ "$verbose" = "true" ]]; then
-        echo "process_tile: [END] Finished processing $z-$x-$y"
+    # Check if sEncoding is valid
+    if [[ "$sEncoding" != "mapbox" && "$sEncoding" != "terrarium" ]]; then
+        usage_message
+        echo "Error: --sEncoding must be either 'mapbox' or 'terrarium'." >&2
+        exit 1 # Return non-zero on error
     fi
+
+    # Return the values as a single string
+    echo "$oMinZoom $demUrl $oDir $increment $sMaxZoom $sEncoding $oMaxZoom $verbose $processes $minx $miny $maxx $maxy"
+    return 0 # return zero for success
 }
 
-# Function 2: Generate all tiles at and above zoom level
+# Function to generate all tiles at and above a zoom level
 process_tile() {
     local programOptions="$0"
     local zoom_level="$1"
     local x_coord="$2"
     local y_coord="$3"
+    #echo "0: $0"
+    #echo "1: $1"
+    #echo "2: $2"
+    #echo "3: $4"
 
-    read oMinZoom demUrl oDir increment sMaxZoom sEncoding oMaxZoom verbose <<<"$programOptions"
+    read oMinZoom demUrl oDir increment sMaxZoom sEncoding oMaxZoom verbose processes <<<"$programOptions"
 
     if [[ "$verbose" = "true" ]]; then
         echo "process_tile: [START] Processing tile - Zoom: $zoom_level, X: $x_coord, Y: $y_coord, oMaxZoom: $oMaxZoom"
@@ -204,9 +258,61 @@ generate_tile_coordinates() {
     return
 }
 
+# Function to convert a bounding box to tile coordinates
+bbox_to_tiles() {
+    local minx=$1
+    local miny=$2
+    local maxx=$3
+    local maxy=$4
+    local zoom=$5
+
+    local tiles=""
+
+    local tempminx=$minx
+    local tempmaxx=$maxx
+    
+    local xtilemin=$(echo "($tempminx + 180) / 360 * 2^$zoom" | bc | awk '{print int($1)}')
+    local ytilemin=$(echo "(1 - l((s($miny * 3.141592653589793 / 180) + 1/c(($miny * 3.141592653589793 / 180)))) / 3.141592653589793 ) / 2 * 2^$zoom" | bc -l | awk '{print int($1)}')
+    local xtilemax=$(echo "($tempmaxx + 180) / 360 * 2^$zoom" | bc | awk '{print int($1)}')
+    local ytilemax=$(echo "(1 - l((s($maxy * 3.141592653589793 / 180) + 1/c(($maxy * 3.141592653589793 / 180)))) / 3.141592653589793 ) / 2 * 2^$zoom" | bc -l | awk '{print int($1)}')
+    
+    
+    if [[ "$ytilemin" -gt "$ytilemax" ]]; then
+        local temp="$ytilemin"
+        ytilemin="$ytilemax"
+       ytilemax="$temp"
+    fi
+
+    for ((y = ytilemin; y <= ytilemax; y++)); do
+        for ((x = xtilemin; x <= xtilemax; x++)); do
+            tiles+="$zoom $x $y "
+        done
+    done
+        
+     echo "$tiles"
+}
+export -f bbox_to_tiles
+
+# Function to run the 'pyramid' command.
+run_option_1() {
+    local programOptions="$1"
+    read x y z demUrl sEncoding sMaxZoom increment oMaxZoom oDir verbose processes <<<"$programOptions"
+
+    if [[ "$verbose" = "true" ]]; then
+        echo "process_tile: [START] Processing tile - Zoom: $z, X: $x, Y: $y, oMaxZoom: $oMaxZoom"
+    fi
+
+    npx tsx ./src/generate-countour-tile-pyramid.ts --x "$x" --y "$y" --z "$z" --demUrl "$demUrl" --sEncoding "$sEncoding" --sMaxZoom "$sMaxZoom" --increment "$increment" --oMaxZoom "$oMaxZoom" --oDir "$oDir"
+
+    if [[ "$verbose" = "true" ]]; then
+        echo "process_tile: [END] Finished processing $z-$x-$y"
+    fi
+}
+
+# Function to run the 'zoom' command.
 run_option_2() {
     local programOptions="$1"
-    read oMinZoom demUrl oDir increment sMaxZoom sEncoding oMaxZoom verbose <<<"$programOptions"
+    read oMinZoom demUrl oDir increment sMaxZoom sEncoding oMaxZoom verbose processes <<<"$programOptions"
 
     echo "Source File: $demUrl"
     echo "Source Max Zoom: $sMaxZoom"
@@ -232,7 +338,7 @@ run_option_2() {
             fi
         }
         trap trap_return INT TERM
-        echo "$tile_coords_str" | xargs -P 8 -n 3 bash -c 'process_tile "$1" "$2" "$3"' "$programOptions"
+        echo "$tile_coords_str" | xargs -P "$processes" -n 3 bash -c 'process_tile "$1" "$2" "$3"' "$programOptions"
         if [[ "$verbose" = "true" ]]; then
             echo "Main: [INFO] Finished tile processing for zoom level $oMinZoom"
         fi
@@ -242,6 +348,47 @@ run_option_2() {
     fi
 
     echo "Main: [END] Finished processing all tiles at zoom level $oMinZoom."
+}
+
+# Function to run the 'bbox' command.
+run_option_3() {
+    local programOptions="$1"
+    read oMinZoom demUrl oDir increment sMaxZoom sEncoding oMaxZoom verbose processes minx miny maxx maxy <<<"$programOptions"
+
+    echo "Source File: $demUrl"
+    echo "Source Max Zoom: $sMaxZoom"
+    echo "Source Encoding: $sEncoding"
+    echo "Output Directory: $oDir"
+    echo "Output Min Zoom: $oMinZoom"
+    echo "Output Max Zoom: $oMaxZoom"
+    echo "Contour Increment: $increment"
+    echo "Main: [START] Processing tiles."
+
+    tile_coords_str=$(bbox_to_tiles "$minx" "$miny" "$maxx" "$maxy" "$oMinZoom")
+	echo "tiles: $tile_coords_str"
+
+    if [[ $? -eq 0 ]]; then
+        if [[ "$verbose" = "true" ]]; then
+            echo "Main: [INFO] Starting tile processing for bounding box"
+        fi
+        #Ensure xargs only runs if it doesn't receive a signal
+        trap_return() {
+            if [ $? -ne 0 ]; then
+                echo "Exiting..." >&2
+                exit 1
+            fi
+        }
+        trap trap_return INT TERM
+        echo "$tile_coords_str" | xargs -P "$processes" -n 3 bash -c 'process_tile "$1" "$2" "$3"' "$programOptions"
+        if [[ "$verbose" = "true" ]]; then
+            echo "Main: [INFO] Finished tile processing for bounding box"
+        fi
+    else
+        echo "Error generating tiles" >&2
+        exit 1
+    fi
+
+    echo "Main: [END] Finished processing all tiles in bounding box."
 }
 
 # --- Main Script ---
@@ -256,7 +403,7 @@ shift
 trap 'kill $(jobs -p); exit 1' INT TERM
 
 case "$function" in
-"generate-tile-pyramid")
+"pyramid")
     programOptions=$(parse_arguments_option_1 "$@")
     ret=$? # capture exit status
     if [[ "$ret" -ne 0 ]]; then
@@ -265,7 +412,7 @@ case "$function" in
     fi
     run_option_1 "$programOptions"
     ;;
-"generate-zoom-level")
+"zoom")
     programOptions=$(parse_arguments_option_2 "$@")
     ret=$? # capture exit status
     if [[ "$ret" -ne 0 ]]; then
@@ -273,6 +420,15 @@ case "$function" in
         exit "$ret"
     fi
     run_option_2 "$programOptions"
+    ;;
+"bbox")
+    programOptions=$(parse_arguments_option_3 "$@")
+    ret=$? # capture exit status
+    if [[ "$ret" -ne 0 ]]; then
+        # Error message will already be handled above
+        exit "$ret"
+    fi
+    run_option_3 "$programOptions"
     ;;
 *)
     echo "Invalid function: $function" >&2
