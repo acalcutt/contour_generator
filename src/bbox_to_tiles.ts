6125f425560aab2eead321a052c50067bb4a8510
HarelM <harel.mazor@gmail.com>
2025-01-15 22:50:24 +0200

    Change file to use regular methods.

--- src/bbox_to_tiles.ts
+++ src/bbox_to_tiles.ts
@@ -1,35 +1,31 @@
 export type Tile = [number, number, number];
 
-const calculateTileRangeForBounds = (
+function convertCoordinatesToTiles(lon: number, lat: number, zoom: number) {
+  const n = Math.pow(2, zoom);
+  const x = Math.floor(((lon + 180) / 360) * n);
+  const y = Math.floor(
+    ((1 -
+      Math.log(
+        Math.tan((lat * Math.PI) / 180) + 1 / Math.cos((lat * Math.PI) / 180),
+      ) /
+        Math.PI) /
+      2) *
+      n,
+  );
+  return { x, y };
+}
+
+function calculateTileRangeForBounds(
   bounds: [number, number, number, number],
   zoom: number,
-) => {
+) {
   const [minLon, minLat, maxLon, maxLat] = bounds;
-
-  const convertCoordinatesToTiles = (
-    lon: number,
-    lat: number,
-    zoom: number,
-  ) => {
-    const n = Math.pow(2, zoom);
-    const x = Math.floor(((lon + 180) / 360) * n);
-    const y = Math.floor(
-      ((1 -
-        Math.log(
-          Math.tan((lat * Math.PI) / 180) + 1 / Math.cos((lat * Math.PI) / 180),
-        ) /
-          Math.PI) /
-        2) *
-        n,
-    );
-    return { x, y };
-  };
 
   const { x: minX, y: maxY } = convertCoordinatesToTiles(minLon, minLat, zoom);
   const { x: maxX, y: minY } = convertCoordinatesToTiles(maxLon, maxLat, zoom);
 
   return { minX, minY, maxX, maxY };
-};
+}
 
 export function bboxToTiles(
   minx: number,
